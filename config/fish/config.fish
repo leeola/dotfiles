88d5066dc77dac8215c5d460af17ea57481df691
Lee Olayvar <leeolayvar@gmail.com>
2013-09-29 18:14:55 +0000

    Fish Ux Vars
    Description in the comment.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,7 +1,10 @@
 
-
-set PATH ~/.bin ~/.n/bin $PATH
-set -x N_PREFIX ~/.n
+# I don't quite understand the scoping rules for -x, -Ux, -gx, etc. I should
+# really read the docs on this one of these days lol. At any rate, i was
+# having some trouble in tmux with my variables being exposed, and -Ux seems
+# to have solved the issue.
+set -Ux PATH ~/.bin ~/.n/bin $PATH
+set -Ux N_PREFIX ~/.n
 
 # This set of stty code has all been commented out due to
 # the fact that stty has issues with Fish. Instead, i run
