75a0d250a5d1a608c515ea6e61703a79a353555d
Lee Olayvar <leeolayvar@gmail.com>
2015-08-23 09:27:13 -0700

    golangtools: Removed gpm / gvp, left gpm-link
    I'm switching to vendoring, so gpm/gvp are less useful. With that
    said, gpm-link can provde to be useful for adding namespaced symlinks

--- core/install/golang-tools.sh
+++ core/install/golang-tools.sh
@@ -8,36 +8,10 @@ echo "[golang-tools.sh] ================== Installing"
 # ### godeps
 go get github.com/tools/godep
 
-# ### gpm
-cd /tmp
-git clone https://github.com/pote/gpm.git && cd gpm
-git checkout v1.2.3
-./configure
-make install
-
-# ### gpm-all
-cd /tmp
-git clone https://github.com/pote/gpm-all.git
-cd gpm-all
-./configure
-make install
-
 # ### gpm-link
 git clone https://github.com/elcuervo/gpm-link.git /tmp/gpm-link
 cd /tmp/gpm-link
 make install
-
-# ### gvp
-cd /tmp
-git clone https://github.com/pote/gvp.git && cd gvp
-git checkout v0.1.0
-./configure
-make install
-cd /tmp
-
-# ### gvp-fish
-git clone https://github.com/leeolayvar/gvp-fish && cd gvp-fish
-cp bin/gvp-fish /usr/local/bin
 
 # ### goimports
 go get github.com/bradfitz/goimports
