30b6935809a776c1f2a39d9849d6d08a02b915e0
Lee Olayvar <leeolayvar@gmail.com>
2015-08-23 09:36:23 -0700

    golang-tools: Moved the bins to usr/local
    In preparation for the new GOPATH

--- core/install/golang-tools.sh
+++ core/install/golang-tools.sh
@@ -7,23 +7,30 @@ echo "[golang-tools.sh] ================== Installing"
 
 # ### godeps
 go get github.com/tools/godep
+mv /go/bin/godep /usr/local/bin/godep
 
 # ### gpm-link
 git clone https://github.com/elcuervo/gpm-link.git /tmp/gpm-link
 cd /tmp/gpm-link
 make install
+rm -rf /tmp/gpm-link
 
 # ### goimports
 go get github.com/bradfitz/goimports
+mv /go/bin/goimports /usr/local/bin/goimports
 
 # ### gocode
 go get github.com/nsf/gocode
+mv /go/bin/gocode /usr/local/bin/gocode
 
 # ### godef
 go get code.google.com/p/rog-go/exp/cmd/godef
+mv /go/bin/godef /usr/local/bin/godef
 
 # ### go oracle
-go get golang.org/x/tools/oracle
+go get golang.org/x/tools/cmd/oracle
+mv /go/bin/oracle /usr/local/bin/oracle
 
 # ### gorename
+# Not sure why this is disabled?
 #go get code.google.com/p/go.tools/cmd/gorename
