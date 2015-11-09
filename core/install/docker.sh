a7fa0374f5740d76b6d6c29378035bec27dd51e9
Lee Olayvar <leeolayvar@gmail.com>
2015-11-08 19:56:04 -0800

    docker: Pinned docker to v1.9.0

--- core/install/docker.sh
+++ core/install/docker.sh
@@ -7,5 +7,5 @@ set -e
 echo "[docker.sh] ================== Installing"
 
 yum install -y libvirt libvirt-client python-virtinst
-curl -Lo /usr/local/bin/docker https://get.docker.io/builds/Linux/x86_64/docker-latest
+curl -Lo /usr/local/bin/docker https://get.docker.io/builds/Linux/x86_64/docker-1.9.0
 chmod +x /usr/local/bin/docker
