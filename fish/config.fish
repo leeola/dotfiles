b25a78846faa4aef0edcddb45eccc2a9706b52de
Lee Olayvar <leeola@fastmail.com>
2020-10-04 16:04:41 -0700

    fish: include DOTFILES env

---
+++ fish/config.fish
@@ -0,0 +1,3 @@
+
+# A central location for any references to the dotfiles location.
+set --export DOTFILES ~/dotfiles
--- home.nix
+++ home.nix
@@ -26,6 +26,7 @@
         # An after-file hook to run `kak -e plug-install` could.. in theory..
         # mutate what it wants. Though the output wouldn't be deterministic.. hmm
         # ".config/kak/plugins/plug.kak".source = plug_kak;
+        ".config/fish/config.fish".source = ./fish/config.fish;
         ".config/fish/functions/dot.fish".source = ./fish/functions/dot.fish;
         ".tmux.conf".source = ./.tmux.conf;
     };
