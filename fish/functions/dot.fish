8fc1f344f9cd998dac2bbc13eb39f8b5c397d702
Lee Olayvar <leeola@fastmail.com>
2020-10-04 16:12:44 -0700

    dot: don't hardcode dotfiles location

--- fish/functions/dot.fish
+++ fish/functions/dot.fish
@@ -1,16 +1,21 @@
 function dot
+  if not set -q DOTFILES
+    echo "error: missing required DOTFILES env"
+    return 1
+  end
+
   switch $argv[1]
   case git kak
     # plain git and kak functionality
-    env -C ~/projects/dotfiles $argv
-  case st ci ls au
+    env -C $DOTFILES $argv
+  case st ci ls au df dc co
     # git shortcuts
-    env -C ~/projects/dotfiles git $argv
+    env -C $DOTFILES git $argv
   case home
     # a shortcut to edit the home config
-    env -C ~/projects/dotfiles kak home.nix
+    env -C $DOTFILES kak home.nix
   case switch
-    env -C ~/projects/dotfiles sudo nixos-rebuild switch --flake .#pc
+    env -C $DOTFILES sudo nixos-rebuild switch --flake .#pc
   case "*"
     echo "unknown dot command: "$argv[1]
     return 1
