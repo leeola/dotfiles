2fe2443e701b5b7e15d7f0ec235bfa5c4ad90e55
Lee Olayvar <leeola@fastmail.com>
2021-08-28 13:23:18 -0700

    fish: add dot boot

--- fish/functions/dot.fish
+++ fish/functions/dot.fish
@@ -21,6 +21,8 @@ function dot
     env -C $DOTFILES sudo nixos-rebuild switch --flake .#
   case test
     env -C $DOTFILES sudo nixos-rebuild test --flake .#
+  case boot
+    env -C $DOTFILES sudo nixos-rebuild boot --flake .#
   case "*"
     echo "unknown dot command: "$argv[1]
     return 1
