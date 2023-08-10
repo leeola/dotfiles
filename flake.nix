318106187bd97b499dd150dffa0a7f6359abfc8a
Lee Olayvar <leeola@fastmail.com>
2023-08-10 12:05:05 -0700

    feat(mbp2017): add disabled mbp

--- flake.nix
+++ flake.nix
@@ -2,6 +2,10 @@
   description = "A very basic flake";
 
   inputs = {
+      system.url = "github:NixOS/nixpkgs/nixos-23.05";
+      adev.url = "github:NixOS/nixpkgs/nixos-23.05";
+      slow.url = "github:NixOS/nixpkgs/nixos-23.05";
+
       nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
       nixpkgs-kak.url = "github:NixOS/nixpkgs/nixos-unstable";
       obsidian.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -27,9 +31,19 @@
         url = "github:nix-community/nixd";
         inputs.nixpkgs.follows = "obsidian";
       };
+
+      darwin-nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-21.11-darwin";
+      darwin.url = "github:lnl7/nix-darwin/master";
+      darwin.inputs.nixpkgs.follows = "darwin-nixpkgs";
   };
 
-  outputs = { home-manager, nixpkgs, plug_kak, nixpkgs-kak, obsidian, nix_lsp_nil, nix_lsp_nixd, ... }:
+  outputs = {
+    home-manager, nixpkgs, plug_kak, nixpkgs-kak, obsidian, nix_lsp_nil, nix_lsp_nixd,
+    
+    darwin, darwin-nixpkgs,
+
+     ...
+  }:
   let
     obsidian-pkgs = import obsidian {
       system = "x86_64-linux";
@@ -102,33 +116,57 @@
     # };
     #kak = nixpkgs-kak.packages.x86_64.kakoune;
     #ruby-1-2-3 = nixpkgs-kak.legacyPackages.x86_64.ruby;
-    nixosConfigurations = {
-      desk = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit nixpkgs-kak; inherit obsidian; };
-        modules = [
-          	  ./system/desk/configuration.nix
-          	  home-manager.nixosModules.home-manager
-          	  {
-                  home-manager.useGlobalPkgs = true;
-                  home-manager.useUserPackages = true;
-                  home-manager.users.lee = import ./home.nix {
-                    plug_kak = plug_kak;
-                    nixpkgs-kak = nixpkgs-kak;
-                    obsidian = obsidian-pkgs;
-                    nix_lsp_nil = nix_lsp_nil.packages.x86_64-linux.nil;
-                    nix_lsp_nixd = nix_lsp_nixd.packages.x86_64-linux.nixd;
-                  };
-                  # home-manager.users.lee = import ./home.nix;
-          	  }
-        ];
-      };
-      closet = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-      	  ./system/closet/configuration.nix
-        ];
-      };
+    nixosConfigurations.desk = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit nixpkgs-kak; inherit obsidian; };
+      modules = [
+        	  ./system/desk/configuration.nix
+        	  home-manager.nixosModules.home-manager
+        	  {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.lee = import ./home.nix {
+                  plug_kak = plug_kak;
+                  nixpkgs-kak = nixpkgs-kak;
+                  obsidian = obsidian-pkgs;
+                  nix_lsp_nil = nix_lsp_nil.packages.x86_64-linux.nil;
+                  nix_lsp_nixd = nix_lsp_nixd.packages.x86_64-linux.nixd;
+                };
+        	  }
+      ];
     };
+    nixosConfigurations.closet = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+    	  ./system/closet/configuration.nix
+      ];
+    };
+    # darwinConfigurations."mbp2017" = darwin.lib.darwinSystem {
+    #   system = "x86_64-darwin";
+    #   # inputs = { inherit nixpkgs-darwin darwin home-manager-darwin; };
+    #   modules = [
+    #     ./system/mbp2017/darwin-configuration.nix
+    #     home-manager.darwinModules.home-manager {
+    #       home-manager.useGlobalPkgs = true;
+    #       home-manager.useUserPackages = true;
+    #       home-manager.users.lee = import ./system/mbp2017/home.nix {
+    #         # inherit dev_kak_plug dev_kak_fzf dev_kak_lsp;
+    #         #home-manager.extraSpecialArgs = { inherit dev; };
+    #         pkgs = import nixpkgs-darwin {
+    #           system = "x86_64-darwin";
+    #           config = { allowUnfree = true; };
+    #         };
+    #         apps = import apps {
+    #           system = "x86_64-darwin";
+    #           config = { allowUnfree = true; };
+    #         };
+    #         dev = import dev {
+    #           system = "x86_64-darwin";
+    #         };
+    #         helix = helix;
+    #         };
+    #       }
+    #   ];
+    # };
   };
 }
---
+++ system/mbp2017/home.nix
@@ -0,0 +1,31 @@
+{
+  pkgs, apps, dev, dev_kak_plug, dev_kak_fzf, dev_kak_lsp, helix
+}: { ... }: {
+    home.packages = [
+        # pkgs.ripgrep
+        # pkgs.fzf
+        # pkgs.direnv
+        # pkgs.alacritty
+
+        # dev.kakoune
+        # dev.tmux
+        # dev.kak-lsp
+        # helix.packages.x86_64-darwin.helix
+
+        # Can't seem to get these to work... :sus:
+        # apps.blender
+        # apps.discord
+        # pkgs.slack
+    ];
+
+    home.file = {
+        # ".gitconfig".source = ../../.gitconfig;
+        # ".config/kak/kakrc".source = ../../.config/kak/kakrc;
+        # ".config/kak/cuser.kak".source = ../../.config/kak/cuser.kak;
+        # ".config/fish/config.fish".source = ../../fish/config.fish;
+        # ".config/fish/functions/dot.fish".source = ../../fish/functions/dot.fish;
+        # ".config/fish/functions/pbp.fish".source = ../../fish/functions/pbp.fish;
+        # ".config/fish/functions/pbc.fish".source = ../../fish/functions/pbc.fish;
+        # ".tmux.conf".source = ../../.tmux.conf;
+    };
+}
