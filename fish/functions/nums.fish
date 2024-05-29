562977d564dadac8cb64dd9b72dda8121b1f8956
Lee Olayvar <leegit@fastmail.com>
2024-05-29 10:27:33 -0700

    fish: add helper nums func

---
+++ fish/functions/nums.fish
@@ -0,0 +1,16 @@
+# A helper to generate numbers with separators and padding.
+# Args:
+# - start incl
+# - end incl
+# - pad_count
+# - sep chars
+function nums
+    set start (math $argv[1])
+    set end (math $argv[2])
+    set pad_count (test -n "$argv[3]"; and echo "$argv[3]"; or echo "1")
+    set sep (test "$argv[4]"; and echo "$argv[4]"; or echo ",")
+    for i in (seq $start $end)
+        printf "%0*d" $pad_count $i
+        test "$i" != "$end"; and echo -n $sep
+    end
+end
--- home/term.nix
+++ home/term.nix
@@ -15,6 +15,7 @@
     ".config/helix/config.toml".source = ../helix/config.toml;
     ".config/helix/languages.toml".source = ../helix/languages.toml;
     ".config/fish/config.fish".source = ../fish/config.fish;
+    ".config/fish/functions/nums.fish".source = ../fish/functions/nums.fish;
     ".tmux.conf".source = ../tmux/tmux.conf;
     ".config/zellij/config.kdl".source = ../zellij/config.kdl;
     ".config/starship.toml".source = ../starship/starship.toml;
