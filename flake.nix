36fd0d9703f0c83735cdb9b5f4f36b0d1964fb65
Lee Olayvar <leegit@fastmail.com>
2025-06-22 09:13:38 -0700

    nix: update claude, 1.0.31

--- flake.nix
+++ flake.nix
@@ -144,10 +144,10 @@
           overlays = [
             (final: prev: {
               claude-code = prev.claude-code.overrideAttrs (old: rec {
-                version = "1.0.29";
+                version = "1.0.31";
                 src = prev.fetchzip {
                   url = "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-${version}.tgz";
-                  hash = "sha256-IQnDrtHeK1F7+yKtPXvfhb6iZq6IKQsn8p7WY8s3jgg=";
+                  hash = "sha256-DrjR1LHnLXk2cW3zXO1jxc3octITMq6t28E2KJHBxZE=";
                 };
               });
 
